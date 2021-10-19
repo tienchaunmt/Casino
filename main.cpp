19d8b0cbc742d1480ed49da9be91579797ce8676
tienchaunmt <tienchaunmt@gmail.com>
2021-10-19 21:40:33 +0700

    Write Vietnamese with accents

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
+#include <io.h>
+#include <fcntl.h>
 
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  _setmode(_fileno(stdin), _O_U16TEXT);
+  _setmode(_fileno(stdout), _O_U16TEXT);
+  wcout << L"Tiếng việt có dấu" << endl;
   system("pause");
   return 0;
 }
