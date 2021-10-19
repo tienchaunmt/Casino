2a3e1e8eb0144fe12bbfcf9be8a053b5459e63ed
tienchaunmt <tienchaunmt@gmail.com>
2021-10-19 21:51:57 +0700

    Name player

--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@ using namespace std;
 int main() {
   _setmode(_fileno(stdin), _O_U16TEXT);
   _setmode(_fileno(stdout), _O_U16TEXT);
-  wcout << L"Tiếng việt có dấu" << endl;
+  system("cls");
+  wstring playerName;
+  wcout << L"Nhập tên của bạn: ";
+  getline(wcin, playerName);
   system("pause");
   return 0;
 }
